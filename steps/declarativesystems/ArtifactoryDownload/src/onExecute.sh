d7f606ac811980c019d93b7f95851ceb9affe5ff
Geoff Williams <geoff@declarativesystems.com>
2020-11-26 19:40:33 +1100

    better output for artifactory download

--- steps/declarativesystems/ArtifactoryDownload/src/onExecute.sh
+++ steps/declarativesystems/ArtifactoryDownload/src/onExecute.sh
@@ -1,19 +1,33 @@
 artifactoryDownload() {
+  local status
+  local rtId
+  rtId=$(find_step_configuration_value "sourceArtifactory")
+
   local path
   path=$(find_step_configuration_value "path")
 
   local target
   target=$(find_step_configuration_value "target") || basename "$path"
 
-  local rtId
-  rtId=$(find_step_configuration_value "sourceArtifactory")
-  setupJfrogCliRt "$rtId"
+  if [ -n "$rtId" ] && [ -n "$path" ]; then
+    setupJfrogCliRt "$rtId"
 
-  echo "downloading ${path} to ${target}"
-  jfrog rt download "$path" "$target"
+    echo "downloading ${path} to ${target}"
+    jfrog rt download "$path" "$target"
 
-  add_pipeline_variables "res_${step_name}_resourcePath=$(pwd)/${target}"
+    local pipeline_variable
+    pipeline_variable="res_${step_name}_resourcePath=$(pwd)/${target}"
+    echo "adding pipeline variable: ${pipeline_variable}"
+    add_pipeline_variables "res_${step_name}_resourcePath=$(pwd)/${target}"
+  else
+    echo "one or more parameters missing:"
+    echo "  sourceArtifactory:${rtId}"
+    echo "  path:${path}"
+    echo "  target:${target}"
+    status=1
+  fi
 
+  return "$status"
 }
 
 
