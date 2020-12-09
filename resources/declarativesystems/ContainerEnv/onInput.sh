9640bb964a1389594305abeb8f1594a31e61209d
Geoff Williams <geoff@declarativesystems.com>
2020-12-10 10:42:23 +1100

    ordering

--- resources/declarativesystems/ContainerEnv/onInput.sh
+++ resources/declarativesystems/ContainerEnv/onInput.sh
@@ -250,6 +250,13 @@ EOF
 # login to artifactory and recover container state
 restore_container_env_state() {
   resourceName=$1
+
+  # artifactory setup
+  local rtId
+  rtId=$(find_resource_variable "$resourceName" "sourceArtifactory")
+  setupArtifactoryPodman "$rtId"
+
+  echo "attempting container state recovery"
   restore_run_files containerStateTarball "$containerStateTarball"
   if [ -f "$containerStateTarball" ] ; then
     local containerStateTarballSize
@@ -257,11 +264,6 @@ restore_container_env_state() {
     tar -zxf "$containerStateTarball" -C /
     echo "restored container state (${containerStateTarballSize}MB)"
   fi
-
-  # artifactory setup
-  local rtId
-  rtId=$(find_resource_variable "$resourceName" "sourceArtifactory")
-  setupArtifactoryPodman "$rtId"
 }
 
 execute_command restore_container_env_state "%%context.resourceName%%"
--- resources/declarativesystems/ContainerEnv/src/onInput.sh
+++ resources/declarativesystems/ContainerEnv/src/onInput.sh
@@ -1,6 +1,13 @@
 # login to artifactory and recover container state
 restore_container_env_state() {
   resourceName=$1
+
+  # artifactory setup
+  local rtId
+  rtId=$(find_resource_variable "$resourceName" "sourceArtifactory")
+  setupArtifactoryPodman "$rtId"
+
+  echo "attempting container state recovery"
   restore_run_files containerStateTarball "$containerStateTarball"
   if [ -f "$containerStateTarball" ] ; then
     local containerStateTarballSize
@@ -8,11 +15,6 @@ restore_container_env_state() {
     tar -zxf "$containerStateTarball" -C /
     echo "restored container state (${containerStateTarballSize}MB)"
   fi
-
-  # artifactory setup
-  local rtId
-  rtId=$(find_resource_variable "$resourceName" "sourceArtifactory")
-  setupArtifactoryPodman "$rtId"
 }
 
 execute_command restore_container_env_state "%%context.resourceName%%"
