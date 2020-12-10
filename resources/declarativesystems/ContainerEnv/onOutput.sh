d0643f4f89749ab4b181e7a5ae153870303e1b73
Geoff Williams <geoff@declarativesystems.com>
2020-12-10 11:10:08 +1100

    support for cleaning finished builds

--- resources/declarativesystems/ContainerEnv/onOutput.sh
+++ resources/declarativesystems/ContainerEnv/onOutput.sh
@@ -250,6 +250,14 @@ EOF
 # save state of containers, less any auth credentials (in /run usually)
 save_container_env_state() {
   resourceName=$1
+
+  local clean
+  clean=$(find_resource_variable "$resourceName" "clean")
+  if [ "$clean" = true ] ; then
+    echo "cleaning container state"
+    rm -rf "$containerStorageDir/*"
+  fi
+
   echo "compressing and saving files for next step"
   if [ -d "$containerStorageDir" ] ; then
     tar -zcf "$containerStateTarball" "$containerStorageDir"
